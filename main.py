02cddc7c0a3d6d6f1e1ffc38fab9320b8de59df5
Ted Win7 <tedsunnyday@gmail.com>
2013-09-07 16:57:18 -0400

    basic template ok

--- main.py
+++ main.py
@@ -14,12 +14,81 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
+import jinja2
+
+from google.appengine.api import users
+import cgi
+import json
+import os
+from pprint import pprint
+from google.appengine.ext import ndb
+from datetime import date 
+
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '')))
+
+
+class Event(ndb.Model):
+    eName = ndb.StringProperty()
+    eLocation = ndb.StringProperty()
+    eDate = ndb.DateProperty()
+    eTime = ndb.StringProperty()
+    eDetails = ndb.TextProperty()
+
+
+class Helper():
+    def __init__(self):
+        pass
+
+    @classmethod
+    def getListDummy(cls,n):
+        return [Event(eName="eName %d" % (i,), eDate=date(2013,9,20), eTime="1:00 PM - 4:00 PM", eLocation="UCS 305", eDetails="What courses in the Yale College Programs...") for i in xrange(0,n)]
+    
+    @classmethod
+    def getListWeb(cls,n):
+        pass
+    
+    @classmethod
+    def getListNDB(cls,n):
+        pass
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        if user:
+            pass
+            #self.response.out.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+            user = users.get_current_user()
+
+        eventList = Helper.getDummyList(100)
+        template_values = {
+            'name': 'user.nickname()',
+            'dict':{'a':user.nickname(),'b':'bb'},
+            'eventList':eventList
+        }
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+class GetAllEvents(webapp2.RequestHandler):
+    def post(self):
+        events = [{'eTime':'2013-aug-20','eName':'orientation'},{'eTime':'2013-aug-21','eName':'reg'}]
+        jsonStr = json.dumps(events)
+        self.response.out.write(jsonStr)
+
+
+class AddToCalendar(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/add', AddToCalendar),
+    ('/getallevents', GetAllEvents)
 ], debug=True)
